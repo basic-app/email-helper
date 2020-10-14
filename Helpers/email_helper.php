394e56e355ba26af44b4544e7cae63f76abe1e31
basic-app <dev@basic-app.com>
2020-10-14 21:09:26 +0300

    n/a

---
+++ Helpers/email_helper.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @author Basic App Dev Team <dev@basic-app.com>
+ * @license MIT
+ * @link https://basic-app.com
+ */
+if (!function_exists('send_email'))
+{
+    function send_email($email, array $options = [], &$error = null)
+    {
+        $email->initialize($options);
+
+        $return = $email->send();
+
+        if (!$return)
+        {
+            if (CI_DEBUG)
+            {
+                $error = $email->printDebugger([]); 
+            }
+            else
+            {
+                $error = 'There was an error sending your message.';
+            }
+        }
+
+        return $return;
+    }
+}
+
+if (!function_exists('compose_email'))
+{
+    function compose_email(string $view, array $params = [])
+    {
+        $message = view($view, $params, ['saveData' => false]);
+
+        $view = Services::renderer();
+
+        $data = $view->getData();
+
+        $subject = $data['subject'] ?? null;
+
+        $mailType = $data['mailType'] ?? 'html';
+
+        $email = single_service('email');
+
+        $email->initialize(['mailType' => $mailType]);
+
+        if ($subject)
+        {
+            $email->setSubject($subject);
+        }
+
+        $email->setMessage($message);
+
+        return $email;
+    }
+}
+
+/**
+ * Parse string "User 1 <user@example.com>, user2@example.com" to array ["user@example.com>" => "User 1", user2@example.com]
+ */
+if (!function_exists('parse_recipients'))
+{
+    function parse_recipients(string $string) : array
+    {
+        $return = [];
+
+        foreach(explode(',', $string) as $segment)
+        {
+            $segment = trim($segment);
+
+            if (preg_match('|(.*)' . preg_quote('<', '|'). '(.+)' . preg_quote('>', '|') .'|', $segment, $matches))
+            {
+                $to_name = $matches[1];
+
+                $to_email = $matches[2];
+
+                $to_name = trim($to_name);
+
+                $return[$email] = $name;
+            }
+            else
+            {
+                $return[] = $string;
+            }
+        }
+
+        return $return;
+    }
+}
